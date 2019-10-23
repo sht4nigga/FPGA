f6745a997bf58c8ee05a869265394aa05d36f82c
sht4nigga <56717043+sht4nigga@users.noreply.github.com>
2019-10-23 16:45:14 +0300

    Last update
    Добавлено описание программы

--- counter1_tb.v
+++ counter1_tb.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 module Kruglikov_tb();
-localparam CLK_PERIOD =2;
+localparam CLK_PERIOD =2;   // Pulse length
 
 reg clk;
 reg rst;
@@ -9,19 +9,19 @@ reg rst;
 always 
 begin
   clk <= 1'b0;
-  #(CLK_PERIOD / 2);
+  #(CLK_PERIOD / 2);        // Zero level duration of clk signal
   clk <= 1'b1;
-  #(CLK_PERIOD / 2);
+  #(CLK_PERIOD / 2);        // One level duration of clk signal
 end
 
 //	Set reset signal
 initial 
 begin
-	rst <= 1'b0;
+	rst <= 1'b0;              // ZERO level duration of Reset signal
 	#10;
-	rst <= 1'b1;
+	rst <= 1'b1;              // ONE level duration of Reset signal
 	#50;
-	rst <= 1'b0;
+	rst <= 1'b0;              // ZERO level duration of Reset signal
 	#200;
     
 	#2000;
@@ -30,11 +30,11 @@ end
 
 always @(posedge clk) 
 begin
-if (rst == 1'b1);
+if (rst == 1'b1);               // Synchronous Reset
 end
 wire strb;
 wire [3:0] Qount;
-presc dut(
+presc dut(                      // Connecting ports
     .clk(clk),
     .rst(rst),  
     .Qount(Qount),
