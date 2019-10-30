dad70fb5e15fca058c0acf88cb229948c8c7222b
sht4nigga <ilin.e@milandr.ru>
2019-10-30 17:00:23 +0300

    ава
    авва

--- Transceiver/Serializer/srlzr.v
+++ Transceiver/Serializer/srlzr.v
@@ -7,11 +7,11 @@ works in the next mode: parallel-in and serial-out
 module serializer_PISO (
     input clk,
 	input rst,
-	input putting,								// Loading(writing) data to the registers
-	input [7:0] data_in,							// 8-bit input data
-	input buff,								// temporary buffer	 
+	input putting,											// Loading(writing) data to the registers
+	input [7:0] data_in,									// 8-bit input data
+	input buff,											// temporary buffer	 
 
-    output srl_out								// serial output data
+    output srl_out											// serial output data
 );
 
 always @(posedge clk)
@@ -19,14 +19,14 @@ always @(posedge clk)
 begin
 	if (rts)
 	begin
-		//data_in<= 0;							// Resetting the input data to ZERO
-		buff<= 0;							// Resetting the temporary data to ZERO
+		//data_in<= 0;									// Resetting the input data to ZERO
+		buff<= 0;											// Resetting the temporary data to ZERO
 	end
 	
 	else
 	if (putting)
 	begin
-	buff<= data_in								// loading "data_in" to the 8-bit register
+	buff<= data_in										// loading "data_in" to the 8-bit register
 	end
 end
 
