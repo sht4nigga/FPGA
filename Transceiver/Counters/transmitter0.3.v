f9df0b1dff9d3fa165cd45b4b5ba7c9d17c7c5a0
sht4nigga <56717043+sht4nigga@users.noreply.github.com>
2019-10-30 21:05:47 +0300

    Update transmitter0.3.v

--- Transceiver/Counters/transmitter0.3.v
+++ Transceiver/Counters/transmitter0.3.v
@@ -1,10 +1,10 @@
 
 module transceiver (
 
-    input clk,
-    input rst,											// start counting
-	input trfr_prm,  									// transfer permission
-    output reg [31:0] data_out				        	// transmitting data
+    	input clk,
+    	input rst,								// start counting
+	input trfr_prm,  							// transfer permission
+	output reg [31:0] data_out				        	// transmitting data
 );
 
 
@@ -12,20 +12,20 @@ always @(posedge clk)
 begin
     if (rst)
     begin
-        data_out <= 0;										// synchronous counter reset
+        data_out <= 0;								// synchronous counter reset
     end 
 	
     else
    
-		if (trfr_prm)										// if we starting the counter
+		if (trfr_prm)							// if we starting the counter
         begin
-            data_out <= data_out + 1;		      			// counter launch(incrementing) 
+            data_out <= data_out + 1;		      				// counter launch(incrementing) 
         end
 
         else
         begin
-            data_out <= 0;					 				// the appearance of strobe
-        end									 				// value of the divided frequency
+            data_out <= 0;					 		// the appearance of strobe
+        end									// value of the divided frequency
 	
 end	
 endmodule
