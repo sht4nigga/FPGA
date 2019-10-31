955eea874dc014c63abdb4ead889f293edb54b92
sht4nigga <ilin.e@milandr.ru>
2019-10-31 09:41:51 +0300

    last
    the last one

--- Transceiver/Serializer/srlzr.v
+++ Transceiver/Serializer/srlzr.v
@@ -1,4 +1,4 @@
-
+/*Это есть недоделанный говнокод. Не надо его копировать на текущем этапе*/
 /*
 Serializer, based on a shift register
 works in the next mode: parallel-in and serial-out
@@ -8,12 +8,12 @@ module serializer_PISO #( parameter DATA_WIDTH = 8)
 (
     input clk,
 	input rst,
-	input valid,											// Loading(writing) data to the registers
-	input [DATA_WIDTH -1:0] data_in,						// 8-bit input data
+	input valid,												// Loading(writing) data to the registers
+	input [DATA_WIDTH -1:0] data_in,							// 8-bit input data
 
-    output srl_out											// serial output data
-	output wire ready;										// may transfer ready data
-	input shift;											// Отщелк на события
+    output srl_out												// serial output data
+	output wire ready;											// may transfer ready data
+	input shift;												// Отщелк на события
 );
 
 reg [DATA_WIDTH -1:0] buff;
