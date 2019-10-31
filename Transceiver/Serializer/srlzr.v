b7bf27ab5808273edac7ea06ac494a948e8c0e21
sht4nigga <ilin.e@milandr.ru>
2019-10-31 10:01:56 +0300

    Variables
    Set all the variables

--- Transceiver/Serializer/srlzr.v
+++ Transceiver/Serializer/srlzr.v
@@ -1,19 +1,20 @@
-/*Это есть недоделанный говнокод. Не надо его копировать на текущем этапе*/
-/*
-Serializer, based on a shift register
-works in the next mode: parallel-in and serial-out
-(PISO) mode
+/*Это есть недоделанный говнокод. Не надо его копировать на текущем этапе
+
+Serializer, based on a shift register works in the next 
+mode: parallel-input and serial-output (PISO) mode
 */
 module serializer_PISO #( parameter DATA_WIDTH = 8)
 (
     input clk,
 	input rst,
+	input valid,												// Loading(writing) data to the registers
+	input [DATA_WIDTH -1:0] data_in,							// 8-bit input data
+	input shift;												// Отщелк на события
 	input valid,								// Loading(writing) data to the registers
 	input [DATA_WIDTH -1:0] data_in,					// 8-bit input data
 
-    output srl_out								// serial output data
-	output wire ready;							// may transfer ready data
-	input shift;								// Отщелк на события
+    output srl_out												// serial output data
+	output wire ready;											// may transfer ready data	
 );
 
 reg [DATA_WIDTH -1:0] buff;
