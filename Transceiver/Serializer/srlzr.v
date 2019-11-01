78055b0a83f3ae21a66ea43bea7fad4f7f1addfc
sht4nigga <ilin.e@milandr.ru>
2019-11-01 13:23:30 +0300

    01.11.2019
    Reg [R:0] = R triggers

--- Transceiver/Serializer/srlzr.v
+++ Transceiver/Serializer/srlzr.v
@@ -1,35 +1,19 @@
-<<<<<<< HEAD
-/*Это есть недоделанный говнокод. Не надо его копировать на текущем этапе
 
-Serializer, based on a shift register works in the next 
-mode: parallel-input and serial-output (PISO) mode
-=======
-/*Это есть недоделанный говнокод. Не надо его копировать на текущем этапе*/
 /*
 Serializer, based on a shift register
 works in the next mode: parallel-in and serial-out
 (PISO) mode
->>>>>>> cant_push_it
 */
 module serializer_PISO #( parameter DATA_WIDTH = 8)
 (
     input clk,
 	input rst,
-	input valid,												// Loading(writing) data to the registers
-	input [DATA_WIDTH -1:0] data_in,							// 8-bit input data
-<<<<<<< HEAD
-	input shift;												// Отщелк на события
-	input valid,								// Loading(writing) data to the registers
-	input [DATA_WIDTH -1:0] data_in,					// 8-bit input data
+	input valid,											// Loading(writing) data to the registers
+	input [DATA_WIDTH -1:0] data_in,									// 8-bit input data
 
-    output srl_out												// serial output data
-	output wire ready;											// may transfer ready data	
-=======
-
-    output srl_out												// serial output data
+    output srl_out											// serial output data
 	output wire ready;											// may transfer ready data
-	input shift;												// Отщелк на события
->>>>>>> cant_push_it
+	input shift;							Отщелк на события
 );
 
 reg [DATA_WIDTH -1:0] buff;
@@ -39,14 +23,14 @@ always @(posedge clk)
 begin
 	if (rst)
 	begin
-		//data_in<= 0;							// Resetting the input data to ZERO
-		buff<= 0;							// Resetting the temporary data to ZERO
+		//data_in<= 0;									// Resetting the input data to ZERO
+		buff<= 0;											// Resetting the temporary data to ZERO
 	end
 	
 	else
 	if (valid&ready)
 	begin
-	buff<= data_in								// loading "data_in" to the 8-bit register
+	buff<= data_in											// loading "data_in" to the 8-bit register
 	end
 	
 end
