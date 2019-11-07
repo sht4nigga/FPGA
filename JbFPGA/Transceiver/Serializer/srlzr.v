3dfdae3425a4808da329e1008e05ea87a4565137
sht4nigga <ilin.e@milandr.ru>
2019-11-07 10:47:06 +0300

    07.11.2019 (10:45)
    added a scheme of a shift register(PISO mode)

--- JbFPGA/Transceiver/Serializer/srlzr.v
+++ JbFPGA/Transceiver/Serializer/srlzr.v
@@ -8,37 +8,57 @@ module serializer_PISO #( parameter DATA_WIDTH = 8)
 	input rst,
 	input valid,													// Loading(writing) data to the registers
 	input [DATA_WIDTH -1:0] data_in,								// 8-bit input data
-	input shift,													// permits the occasion
+	input shift,													// permits the shift
+	input LOAD,														// then load the data in to the triggers
 
-    output srl_out,												// serial output data
-	output wire ready,											// may transfer data from the "buff" to "srl_out"
-	output tx_active												// shows the data transfering
+    output srl_out,										// serial output data
+	output wire ready,									// may transfer data from the "BUFF" to "srl_out"
+	output TX_active												// shows the data transfering
 );
 
-assign srl_out = buff[0];											// define the buff contents to the serial out
-reg [DATA_WIDTH -1:0] buff;											// temporary local data storage
+assign srl_out = BUFF[0];											// define the BUFF contents to the serial out
+reg [DATA_WIDTH -1:0] BUFF;											// temporary local data storage
+int i, q;															// integer vareables for counting the quantities steps of data_width
 
 always @(posedge clk)
 begin
 
 	if (rst)
 	begin
-		buff<= 0;									// Resetting the temporary data to ZERO	
+		BUFF<= 0;									// Resetting the temporary data to ZERO	
 		ready<= 0;									// Resetting the temporary data to ZERO	
-		tx_active<= 0;								// Resetting the transering signalto ZERO
+		TX_active<= 0;								// Resetting the transering signalto ZERO
 	end
 	
 	else
 	if (valid & ready)
 	begin
-		buff<= data_in;								// loading "data_in" to the 8-bit register
+		BUFF<= data_in;								// loading "data_in" to the 8-bit register
 	end
 	
+		else
+		begin
+			if (LOAD == 1'b1)								// the loading data mode to the registers
+			begin
+				for (i=0, i<=DATA_WIDTH, i=i+1)
+					if( data_in[i] | shift ) <= 0;
+						( LOAD | data_in[i] ) <= 1;
+			end
+			
+			else
+			if (shift == 1'b1)
+			begin
+				for (q=0, q<=DATA_WIDTH, q=q+1)
+					if( data_in[i] | LOAD ) <= 0;
+						( shift | data_in[i] ) <= 1;
+			end
+		end
+		
 	/*Waiting mode*/
 	else
 	if ( ready == 1'b1 )
 	begin
-	data_in <= buff;
+	data_in <= BUFF;
 	end
 	
 end
