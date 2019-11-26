e3d4b632f763afee38b6112a2757465466cb5fdb
sht4nigga <ilin.e@milandr.ru>
2019-11-26 13:33:52 +0300

    s
    s

--- JbFPGA/studie/DeMultiplexer.v
+++ JbFPGA/studie/DeMultiplexer.v
@@ -4,14 +4,14 @@ for conducting the data
 */
 module DeMultiplexer ()
 (
-    input wire [1:0] inA,		       // input line
+    input wire [1:0] A_in,		       // input line
 	input wire [1:0] Select,	   // switch signal
 
-    output wire inB,		// output line	
-	output wire inC		// output line
+    output wire B,		// output line	
+	output wire C		// output line
 );
 
-assign inB = (Select)? 1'b0 : inA;   // B = A, if "S" equal to logic 1, otherwise B = 0
-assign inC = (Select)? inA : 1'b0;   // C = A, if "S" equal to logic 1, otherwise B = 0
+assign B = (Select)? 1'b0 : A_in;   // B = A, if "S" equal to logic 1, otherwise B = 0
+assign C = (Select)? A_in : 1'b0;   // C = A, if "S" equal to logic 1, otherwise B = 0
 
 endmodule
