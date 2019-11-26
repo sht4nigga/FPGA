4f91a815b2f50b47538a9ccd8dfe4e4b8376d617
sht4nigga <ilin.e@milandr.ru>
2019-11-26 16:51:23 +0300

    s
    s

--- JbFPGA/studie/DeMultiplexer_tb.v
+++ JbFPGA/studie/DeMultiplexer_tb.v
@@ -1,26 +1,35 @@
-`timescale 1ns / 1ps
 module DeMultiplexer_tb;
 
 	reg [1:0] inA;		     // input line
-	reg [1:0] Select;		// switch signal
-	reg [1:0] outB;		// output line	
-	reg [1:0] outC;		// output line
+	reg [1:0] Select;	     // switch signal
+	reg [1:0] outB;		     // output line	
+	reg [1:0] outC;		     // output line
 
 integer i;
 wire B;
 wire C;
 
-DeMultiplexer my_DEmux ( 
-Select,
-outB,
-outC,
-inA
+DeMultiplexer my_DEmux ( inA, Select, outB, outC);                   // Connecting ports
 
-)                   // Connecting ports
-(
-    .A(A),
-    .B(B),  
-    .C(C),
-    .S(S)
-);
+    initial 
+    begin
+        begin
+            Select = 0; 
+            inA = 0;     #1;
+            
+            Select = 0; 
+            inA = 1;     #1;
+            
+            Select = 1; 
+            inA = 0;     #1;
+            
+            Select = 1; 
+            inA = 1;     #1;
+            
+            Select = 1;  #1;
+            Select = 2;  #1;
+            Select = 3;  #1;
+            $display("-----------------------------------------");
+        end
+    end
 endmodule
