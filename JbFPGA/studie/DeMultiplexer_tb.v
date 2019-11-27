1c4616349092cf199121b28e33c44d239f1425c7
sht4nigga <ilin.e@milandr.ru>
2019-11-27 11:28:05 +0300

    1
    1

--- JbFPGA/studie/DeMultiplexer_tb.v
+++ JbFPGA/studie/DeMultiplexer_tb.v
@@ -1,34 +1,40 @@
+`timescale 1ns/1ps
+
 module DeMultiplexer_tb;
 
-	reg [1:0] inA;		     // input line
+localparam CLK_PERIOD = 10;
+
+
+    
+    
+	reg [1:0] A_in;		     // input line
 	reg [1:0] Select;	     // switch signal
-	reg [1:0] outB;		     // output line	
-	reg [1:0] outC;		     // output line
-
-integer i;
-wire B;
-wire C;
-
-assign B = outB;
-assign C = outC;
-
-DeMultiplexer my_DEmux ( inA, Select, B, C);                   // Connecting ports
+	wire [1:0] B;		     // output line	
+	wire [1:0] C;		     // output line
 
 
-    initial 
-    begin
+initial 
+    
         begin
-            Select = 0; 
-            inA = 0;     #1;
-            
-            Select = 0; 
-            inA = 1;     #1;
-            
-            Select = 1; 
-            inA = 0;     #1;
-            
-            Select = 1; 
-            inA = 1;     #1;
+             Select <= 0; 
+             A_in <= 0;     #5; 
+                        
+             Select <= 0; 
+             A_in <= 1;     #5;
+             
+             Select <= 1; 
+             A_in <= 0;     #5;
+             
+             Select <= 1; 
+             A_in <= 1;     #5;
         end
-    end
+
+
+DeMultiplexer my_DEmux (
+            .A_in(A_in), 
+            .Select(Select),
+            .outB(B),
+            .outC(C)
+);                   // Connecting ports
+    
 endmodule
