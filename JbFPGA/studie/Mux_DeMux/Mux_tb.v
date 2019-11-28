69bef08fb43545ef0c5645a372c236586e136e42
sht4nigga <ilin.e@milandr.ru>
2019-11-28 14:52:47 +0300

    tb
    added tb

--- JbFPGA/studie/Mux_DeMux/Mux_tb.v
+++ JbFPGA/studie/Mux_DeMux/Mux_tb.v
@@ -2,66 +2,48 @@
 module DeMultiplexer_tb;
 localparam CLK_PERIOD = 10;
 	reg [1:0] A_in;		     // input line
+	reg [1:0] B_in;		     // input line
 	reg Select;	             // switch signal
-	wire [1:0] B;		     // output line	
+	
+	wire [1:0] Yout;		     // output line	
 	wire [1:0] C;		     // output line
 initial 
     begin
         Select <= 0;                    // non switching mode
-        A_in <= 0;     #5;              // no data at the input pin
+        B_in <= 0;     
+		A_in <= 0;     #5;              // no data at the input pin
                                 
-        Select <= 0;                        // non switching mode
-        A_in <= 1;     #5;                  // data at the input pin
-                     
-        Select <= 1;                        // switching mode
-        A_in <= 0;     #5;                  // no data at the input pin
-                     
-        Select <= 1;                            // switching mode
-        A_in <= 1;     #5;                      // data at the input pin      
- //the first cycle-----------------------------------------------
-  
         Select <= 0;                    // non switching mode
-        A_in <= 0;     #5;              // no data at the input pin
-                                
-        Select <= 0;                        // non switching mode
-        A_in <= 1;     #5;                  // data at the input pin
-                     
-        Select <= 1;                        // switching mode
-        A_in <= 0;     #5;                  // no data at the input pin
-                     
-        Select <= 1;                            // switching mode
-        A_in <= 1;     #5;                      // data at the input pin  
- //the second cycle-----------------------------------------------
- 
+        B_in <= 0;     
+		A_in <= 0;     #5;              // no data at the input pin
+		
         Select <= 0;                    // non switching mode
-        A_in <= 0;     #5;              // no data at the input pin
-                                
-        Select <= 0;                        // non switching mode
-        A_in <= 1;     #5;                  // data at the input pin
-                     
-        Select <= 1;                        // switching mode
-        A_in <= 0;     #5;                  // no data at the input pin
-                     
-        Select <= 1;                            // switching mode
-        A_in <= 1;     #5;                      // data at the input pin   
- //the third cycle-----------------------------------------------
- 
+        B_in <= 0;     
+		A_in <= 0;     #5;              // no data at the input pin
+
         Select <= 0;                    // non switching mode
-        A_in <= 0;     #5;              // no data at the input pin
-                                
-        Select <= 0;                        // non switching mode
-        A_in <= 1;     #5;                  // data at the input pin
-                     
-        Select <= 1;                        // switching mode
-        A_in <= 0;     #5;                  // no data at the input pin
-                     
-        Select <= 1;                            // switching mode
-        A_in <= 1;     #5;                      // data at the input pin  
- //the fourth cycle-----------------------------------------------
+        B_in <= 0;     
+		A_in <= 0;     #5;              // no data at the input pin
+		
+        Select <= 0;                    // non switching mode
+        B_in <= 0;     
+		A_in <= 0;     #5;              // no data at the input pin
+		
+        Select <= 0;                    // non switching mode
+        B_in <= 0;     
+		A_in <= 0;     #5;              // no data at the input pin
+		
+        Select <= 0;                    // non switching mode
+        B_in <= 0;     
+		A_in <= 0;     #5;              // no data at the input pin
+		
+        Select <= 0;                    // non switching mode
+        B_in <= 0;     
+		A_in <= 0;     #5;              // no data at the input pin		
     end    
 DeMultiplexer my_DEmux (          /* Connecting ports*/
-            .A_in(A_in), 
             .Select(Select),
-            .outB(B),
-            .outC(C)    );   
+			.A_in(A_in), 
+            .B_in(B_in),
+            .Y(Yout)    );   
 endmodule
