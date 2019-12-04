0e4d41bad54dad1791e3b26ff673867c4082495e
sht4nigga <ilin.e@milandr.ru>
2019-12-04 11:43:10 +0300

    Alu
    Alu final ver

--- mipt/labs/07_cpu_v1/alu.v
+++ mipt/labs/07_cpu_v1/alu.v
@@ -6,10 +6,14 @@ module alu(
     output reg [31:0]res	// the operation result
 );
 /*Problem 1: write operations execution logic here
+--------------------------------------------------
+By getting the signal at the "op" output, choose
+the operation to execute accordingly to current 
+signal. Otherwise get the ZERO from the 
+"res"*/
 
 always @(*) 
 /*
-
 1) if op == 0, the result = scr_a
 2) if op == 1, the result = scr_a + scr_b
 3) for other undefined values of "op" the result = 0
@@ -19,12 +23,12 @@ begin
 	case (op)
 		4'b0000:
 		begin
-			res = scr_a; 
+			res = src_a; 
 		end
 		
 		4'b0010:
 		begin
-			res = scr_a + scr_b;
+			res = src_a + src_b;
 			/* res = scr_a || scr_b;*/
 		end
 		default res = 1'd0;
