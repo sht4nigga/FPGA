c2c9e977c28c555812ca8e9636a82b2a2841b47e
sht4nigga <ilin.e@milandr.ru>
2019-12-04 10:55:04 +0300

    alu
    added alu file

--- mipt/labs/07_cpu_v1/alu.v
+++ mipt/labs/07_cpu_v1/alu.v
@@ -5,20 +5,29 @@ module alu(
 
     output reg [31:0]res	// the operation result
 );
+/*Problem 1: write operations execution logic here
 
 always @(*) 
-/*Problem 1: write operations execution logic here*/
-begin
-	if (op == 4'b0000)
-	begin
-	res <= scr_A;		// returning the "scr_A", as the RESULT of the instruction perfomance
-	end
-	
-	else
-	if(op == 4'b0010)
-	begin
-	res <= scr_a || scr_b;
-	end
-end
+/*
 
+1) if op == 0, the result = scr_a
+2) if op == 1, the result = scr_a + scr_b
+3) for other undefined values of "op" the result = 0
+
+*/
+begin
+	case (op)
+		8'b00000000:
+		begin
+			res = scr_a; 
+		end
+		
+		8'b00000010:
+		begin
+			res = scr_a + scr_b;
+			/* res = scr_a || scr_b;*/
+		end
+		default res = 1'd0;
+	endcase
+end
 endmodule
