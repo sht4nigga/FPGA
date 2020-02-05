0637dd60e027baf5b902e43768d42e4d2b16c3aa
Efim <56717043+sht4nigga@users.noreply.github.com>
2020-02-05 16:21:45 +0300

    Update Mux2ver_tb.v

--- JbFPGA/studie/Mux_DeMux/Mux2ver_tb.v
+++ JbFPGA/studie/Mux_DeMux/Mux2ver_tb.v
@@ -1,53 +1,53 @@
-`timescale 1ns / 1ps
-module serializer_PISO_tb #(parameter DATA_WIDTH_Tb = 8);
+`timescale 1ns/1ps
+module Mux2ver_tb;
+localparam CLK_PERIOD = 10;
+	reg A_in;		     // input line
+	reg B_in;		     // input line
+	reg Select;	             // switch signal
 
-        localparam CLK_PERIOD = 10;
+	wire Yout;		 // output line	
+	integer i;
+	always begin
+        for (i= 1; i<= 12; i=i+1)           // cycle "for" repeats the input configuration "i<= X" times
+        begin
+            Select <= 0;                    // non switching mode
+            A_in <= 0;     
+            B_in <= 0;     #5;              // no data at the input pin
 
-        reg [1:0] tb_clk;
-        reg [1:0] tb_rst;
-		reg [1:0] shift;                           // shifting signal
-		reg [1:0] LOAD;		                     // loading signal
+            Select <= 0;                    // non switching mode
+            A_in <= 0;     
+            B_in <= 1;     #5;              // no data at the input pin
 
-		reg [1:0] srl_out;	                       // output line	
-		reg [DATA_WIDTH_Tb-1:0] data_in_Tb;		// output line
-		
+            Select <= 0;                    // non switching mode
+            A_in <= 1;     
+            B_in <= 0;     #5;              // no data at the input pin
 
-//Setting the tb_clk signal____________________________________________________________________
-always 
-begin
- // S <= 1'b0;
-  //#(S / 2);        // Zero level duration of clk period
-  tb_clk <= 1'b1;
-  #(CLK_PERIOD / 2);        // HIGH level duration of clk period
-end
+            Select <= 0;                    // non switching mode
+            A_in <= 1;     
+            B_in <= 1;     #5;              // no data at the input pin
+    //------------------------------------------------------------------		
+            Select <= 1;                    // non switching mode
+            A_in <= 0;     
+            B_in <= 0;     #5;              // no data at the input pin
 
-//	Set reset signal
-initial 
-begin
-	tb_rst <= 0;              // ZERO level duration of Reset signal
-	#10;
-	tb_rst <= 1'b1;              // ONE level duration of Reset signal
-	#30;
-	tb_rst <= 0;              // ZERO level duration of Reset signal
-	#200;
-    
-	#2000;
-	$finish;
-end
+            Select <= 1;                    // non switching mode
+            A_in <= 0;     
+            B_in <= 1;     #5;              // no data at the input pin
 
-always @(posedge tb_clk) 
-begin
-if (tb_rst == 1'b1);               // Synchronous Reset
-end
-//____________________________________________________________________
+            Select <= 1;                    // non switching mode
+            A_in <= 1;     
+            B_in <= 0;     #5;              // no data at the input pin
 
-/*wire B;
-wire C;
-DeMultiplexer dut ()                   // Connecting ports
-(
-    .A(A),
-    .B(B),  
-    .C(C),
-    .S(S)
-);*/
+            Select <= 1;                    // non switching mode
+            A_in <= 1;     
+            B_in <= 1;     #5;              // no data at the input pin
+        end
+    end
+
+Mux2ver my_mux2ver (          /* Connecting ports*/
+            .Select(Select),
+			.A_in(A_in), 
+            .B_in(B_in),
+            .Y(Yout)    
+            );   
 endmodule
