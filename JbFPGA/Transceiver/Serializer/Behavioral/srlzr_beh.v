33523cc03ae494959285a0500f0d111ca9765e3d
sht4nigga <ilin.e@milandr.ru>
2020-02-18 19:34:46 +0300

    1
    1

--- JbFPGA/Transceiver/Serializer/Behavioral/srlzr_beh.v
+++ JbFPGA/Transceiver/Serializer/Behavioral/srlzr_beh.v
@@ -10,10 +10,9 @@ module piso4_struc #(parameter DATA_WIDTH = 5)
 	output [DATA_WIDTH-1:1] y,                                     // Triggers outputs, wich goes to the next trigger
     output z                                           			// Serial output
 );   
- 
     reg [DATA_WIDTH-1:1] y;
     
-    assign z= y[DATA_WIDTH-1];                                     			// Put the last trigger data to serial output
+    assign z= y[DATA_WIDTH-1];                                     // Put the last trigger data to serial output
     integer i;
 
 always@(posedge clk)
@@ -28,13 +27,6 @@ begin
         begin
             y[i+1]<= ((load && x[i+1]) || (~load && i));
         end
-	end
-     /*y[1]<= ((load && x[1]) || (~load && 1'b0));
-     y[2]<= ((load && x[2]) || (~load && y[1])); 
-     y[3]<= ((load && x[3]) || (~load && y[2]));
-     y[4]<= ((load && x[4]) || (~load && y[3]));*/      
+	end     
 end
-    
-
-
 endmodule
