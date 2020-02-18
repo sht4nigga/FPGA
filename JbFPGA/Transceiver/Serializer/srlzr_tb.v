1a3bc747700dc9f6c7a3f9064a2709c4ed2b275c
sht4nigga <ilin.e@milandr.ru>
2020-02-18 10:26:08 +0300

    nice TB
    works

--- JbFPGA/Transceiver/Serializer/srlzr_tb.v
+++ JbFPGA/Transceiver/Serializer/srlzr_tb.v
@@ -1,76 +1,56 @@
-module serializer_PISO #( parameter DATA_WIDTH = 8)
+`timescale 1ns / 1ps
+module serializer_PISO_tb #(parameter DATA_WIDTH_Tb = 8);
+
+        localparam CLK_PERIOD = 10;
+
+        reg tb_clk;
+        reg tb_rst;
+        reg LOAD_tb;		                     // loading signal
+        
+        wire shift_tb;                           // shifting signal
+        wire ready_tb;		
+//Setting the tb_clk signal____________________________________________________________________
+always 
+begin
+  tb_clk <= 1'b0;
+  #(CLK_PERIOD / 2);               // Zero level duration of clk period
+  tb_clk <= 1'b1;
+  #(CLK_PERIOD / 2);        // HIGH level duration of clk period
+end
+
+//	Set reset signal
+initial 
+begin
+	tb_rst <= 0;              // ZERO level duration of Reset signal
+	#10;
+	tb_rst <= 1'b1;              // ONE level duration of Reset signal
+	#30;
+	tb_rst <= 0;              // ZERO level duration of Reset signal
+	#10;
+    
+	#2000;
+	$finish;
+end
+
+always @(posedge tb_clk) 
+begin
+if (tb_rst == 1'b1);               // Synchronous Reset
+begin
+LOAD_tb<= 1'b1;
+#50;
+LOAD_tb<= 1'b0;
+#50;
+end
+end
+
+serializer_PISO serializer_PISO_tb                    // Connecting ports
 (
-    input clk,
-	input rst,
-	input wire ready,
-	//input wire[DATA_WIDTH -1:0] data_in,				// 8-bit input data
-	input wire LOAD,									// allow load the data in to the triggers
-	
-    output srl_out									    // serial output data
-	
+    .clk(tb_clk),
+    .rst(tb_rst),  
+    //.BUFF(BUFF_tb),
+    //.ready(ready_tb),
+    .LOAD(LOAD_tb)
+    //.TX_active(TX_active_tb),
+    //.shift(shift_tb)
 );
-
-reg [DATA_WIDTH -1:0] BUFF;                             // temporary local data storage		
-reg [DATA_WIDTH -1:0] data_in;
-reg TX_act;                                             // shows the current transfer
-reg iload;
-reg iready;                                             // temporary vatriable for connecting to the LOAD variable
-reg shift;										        // permits the data shift
-reg d_valid;                                            // data valid
-integer i, q, k;								        // integer vareables for counting the steps quantity of data_width
-
-assign LOAD = iload;                                    // varible, which allowa us to load the inpit data in to the SERIALIZER
-assign ready = iready;                                  // temporary variable "iready" for ready
-assign srl_out = BUFF[0];								// define the BUFF contents to the serial out
-
-
-always @(posedge clk)
-begin
-	if (rst)
-	begin
-	    //shift<= 0;								    // Resetting the shifting signal to ZERO
-		//iload<= 0;                                    // Resetting the temporary load variable to ZERO
-		BUFF<= 0;									    // Resetting the temporary data to ZERO		
-		TX_act<= 0;                                     // Resetting the transer signal to ZERO
-		iready<= 0;                                     // Resetting "the data ready to load" variable
-		d_valid<= 0;                                    // Resetting data valid
-	end
-	
-	else
-	if (ready == 1)                                       
-    begin
-		if(LOAD == 1)                                   // After  we got (ready = 1), whaiting for (LOAD = 1)
-		begin
-		  BUFF<= data_in;		                        // to load the data inside the BUFF
-		end
-		
-		else
-		if(LOAD == 0)
-		begin
-		
-		end
-	end
-			
-	else
-	if (ready == 0)								        //data loading mode(to the registers)
-	begin
-		for (i=0; i <= DATA_WIDTH; i=i+1)				// run all the numbers(inputs) of the data_width regs
-		if((data_in[i] | shift) <= 0)				    // "d_i" & "shift" goes through the "OR" gate, so we have a ZERO(shift=0)
-        begin
-            if (LOAD & data_in[DATA_WIDTH])             // "LOAD" & "d_i" goes through the "AND" gate(bitwise)
-            begin					
-                //ready <= 1'b1;
-            end
-        end											    // so we have a HIGH output(LOAD=1)
-    end		
-end
-/*If TX_act is HIGH, thus we are tranfering
-the BUFF temporary data in to the "srl_out"*/
-/*
-if (TX_act == true)
-begin
-BUFF <= srl_out;
-end
-*/
-
 endmodule
