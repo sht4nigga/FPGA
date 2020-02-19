edf7c17cba9daad4e270867784e408fdbc52de9c
sht4nigga <ilin.e@milandr.ru>
2020-02-19 12:46:37 +0300

    1
    1

--- JbFPGA/Transceiver/Serializer/Functional/srlzrLST.v
+++ JbFPGA/Transceiver/Serializer/Functional/srlzrLST.v
@@ -1,76 +1,30 @@
-module serializer_PISO #( parameter DATA_WIDTH = 8)
+module piso #(parameter DATA_WIDTH = 9) 
+
 (
-    input clk,
-	input rst,
-	input wire ready,
-	//input wire[DATA_WIDTH -1:0] data_in,				// 8-bit input data
-	input wire LOAD,									// allow load the data in to the triggers
+    input rst_n, 
+	input clk,
+	input wire load,                             	// Input signals	
+	input [DATA_WIDTH-1:1] x,                       // Input parallel data
 	
-    output srl_out									    // serial output data
-	
-);
+	output [DATA_WIDTH-1:1] y,                      // Triggers outputs, wich goes to the next trigger
+    output z                                            // Serial output
+);   
+    reg [DATA_WIDTH-1:1] y;
+    assign z= y[DATA_WIDTH-1];                          // Put the last trigger data to serial output
+    integer i;
 
-reg [DATA_WIDTH -1:0] BUFF;                             // temporary local data storage		
-reg [DATA_WIDTH -1:0] data_in;
-reg TX_act;                                             // shows the current transfer
-reg iload;
-reg iready;                                             // temporary vatriable for connecting to the LOAD variable
-reg shift;										        // permits the data shift
-reg d_valid;                                            // data valid
-integer i, q, k;								        // integer vareables for counting the steps quantity of data_width
-
-assign LOAD = iload;                                    // varible, which allowa us to load the inpit data in to the SERIALIZER
-assign ready = iready;                                  // temporary variable "iready" for ready
-assign srl_out = BUFF[0];								// define the BUFF contents to the serial out
-
-
-always @(posedge clk)
+always@(posedge clk)
 begin
-	if (rst)
-	begin
-	    //shift<= 0;								    // Resetting the shifting signal to ZERO
-		//iload<= 0;                                    // Resetting the temporary load variable to ZERO
-		BUFF<= 0;									    // Resetting the temporary data to ZERO		
-		TX_act<= 0;                                     // Resetting the transer signal to ZERO
-		iready<= 0;                                     // Resetting "the data ready to load" variable
-		d_valid<= 0;                                    // Resetting data valid
-	end
-	
-	else
-	if (ready == 1)                                       
+    if (rst_n)
+    y<= {DATA_WIDTH{1'b0}};
+    
+    else
+    if(load)
     begin
-		if(LOAD == 1)                                   // After  we got (ready = 1), whaiting for (LOAD = 1)
-		begin
-		  BUFF<= data_in;		                        // to load the data inside the BUFF
-		end
-		
-		else
-		if(LOAD == 0)
-		begin
-		
-		end
-	end
-			
-	else
-	if (ready == 0)								        //data loading mode(to the registers)
-	begin
-		for (i=0; i <= DATA_WIDTH; i=i+1)				// run all the numbers(inputs) of the data_width regs
-		if((data_in[i] | shift) <= 0)				    // "d_i" & "shift" goes through the "OR" gate, so we have a ZERO(shift=0)
+        for(i= 0; i< DATA_WIDTH; i= i+1)
         begin
-            if (LOAD & data_in[DATA_WIDTH])             // "LOAD" & "d_i" goes through the "AND" gate(bitwise)
-            begin					
-                //ready <= 1'b1;
-            end
-        end											    // so we have a HIGH output(LOAD=1)
-    end		
+            y[i+1]<= ((load && x[i+1]) || (~load && i));
+        end
+    end     
 end
-/*If TX_act is HIGH, thus we are tranfering
-the BUFF temporary data in to the "srl_out"*/
-/*
-if (TX_act == true)
-begin
-BUFF <= srl_out;
-end
-*/
-
 endmodule
--- JbFPGA/Transceiver/Serializer/Functional/srlzr_tb.v
+++ JbFPGA/Transceiver/Serializer/Functional/srlzr_tb.v
@@ -1,103 +1,44 @@
-        `timescale 1ns / 1ps
-module serializer_PISO_tb #(parameter DATA_WIDTH_Tb = 8);
+`timescale 1ns / 1ps
+module piso_tb #(parameter DATA_WIDTH = 9);
 
-        localparam CLK_PERIOD = 10;
-
-        reg tb_clk;
-        reg tb_rst;
-        reg LOAD_tb;		                     // loading signal
+        reg clk, rst_n, load;
+        reg [DATA_WIDTH-1:1] x;
         
-        wire shift_tb;                           // shifting signal
-        wire ready_tb;		
-//Setting the tb_clk signal____________________________________________________________________
-always 
+        wire [DATA_WIDTH-1:1] y;
+        wire z;
+   
+initial
 begin
-  tb_clk <= 1'b0;					// Zero level of clk signal
-  #(CLK_PERIOD / 2);				// Zero level duration of clk signal with clk_per/2 length
-  tb_clk <= 1'b1;					// HIGH level of clk signal
-  #(CLK_PERIOD / 2);				// HIGH level duration of clk signal with clk_per/2 length
+    clk = 1'b0;
+    forever
+    #10clk= ~clk;
 end
 
-//	Set reset signal
-initial 
+initial
+    $monitor ("x= %b, y= %b, z= %b", x, y, z);
+
+initial
 begin
-	tb_rst <= 0;              		// ZERO level of the Reset signal
-	#10;							// Zero level duration of the Reset signal
-	tb_rst <= 1'b1;					// HIGH level of the Reset signal
-	#30;							// HIGH level duration of the Reset signal
-	tb_rst <= 0;              		// ZERO level of the Reset signal
-	#10;							// Zero level duration of the Reset signal
+    #0 rst_n = 1'b0;
+       load = 1'b0;
+       x  = {DATA_WIDTH{1'b0}};
+       
+    #3 rst_n = 1'b1;
     
-	#2000;
-	$finish;
-end
-
-always @(posedge tb_clk) 
-begin
-	if (tb_rst == 1'b1);			// Synchronous Reset
-	begin
-		LOAD_tb<= 1'b1;
-		#50;
-		LOAD_tb<= 1'b0;
-		#50;
-	end
-end
-
-serializer_PISO serializer_PISO_tb                    // Connecting ports
-(
-    .clk(tb_clk),
-    .rst(tb_rst),  
-    //.BUFF(BUFF_tb),
-    //.ready(ready_tb),
-    .LOAD(LOAD_tb)
-    //.TX_active(TX_active_tb),
-    //.shift(shift_tb)
-);
-endmodule
-        wire shift_tb;                           // shifting signal
-        wire ready_tb;		
-//Setting the tb_clk signal____________________________________________________________________
-always 
-begin
-  tb_clk <= 1'b0;					// Zero level of clk signal
-  #(CLK_PERIOD / 2);				// Zero level duration of clk signal with clk_per/2 length
-  tb_clk <= 1'b1;					// HIGH level of clk signal
-  #(CLK_PERIOD / 2);				// HIGH level duration of clk signal with clk_per/2 length
-end
-
-//	Set reset signal
-initial 
-begin
-	tb_rst <= 0;              		// ZERO level of the Reset signal
-	#10;							// ZERO level duration of the Reset signal
-	tb_rst <= 1'b1;					// HIGH level of the Reset signal
-	#30;							// HIGH level duration of the Reset signal
-	tb_rst <= 0;              		// ZERO level of the Reset signal
-	#10;							// ZERO level duration of the Reset signal
+    #2 x = {DATA_WIDTH{1'b1}};
+    #3 load = 1'b1;
+    #7 load = 1'b0;
     
-	#2000;
-	$finish;
+    #120 $stop;
 end
 
-always @(posedge tb_clk) 
-begin
-	if (tb_rst == 1'b1);			// Synchronous Reset
-	begin
-		LOAD_tb<= 1'b1;
-		#50;
-		LOAD_tb<= 1'b0;
-		#50;
-	end
-end
+piso inst1 (
 
-serializer_PISO serializer_PISO_tb /* Connecting ports */
-(
-    .clk(tb_clk),
-    .rst(tb_rst),  
-    //.BUFF(BUFF_tb),
-    //.ready(ready_tb),
-    .LOAD(LOAD_tb)
-    //.TX_active(TX_active_tb),
-    //.shift(shift_tb)
+    .rst_n(rst_n),
+    .clk(clk),
+    .load(load),
+    .x(x),
+    .y(y),
+    .z(z)
 );
 endmodule
